bf08ab9817fae1733d7150909dcda16e8f790d22
Marko Leinikka <mleinikka@gmail.com>
2022-01-29 23:04:41 +0200

    feat: Session management

--- pages/personal.tsx
+++ pages/personal.tsx
@@ -2,21 +2,27 @@ import { GetServerSideProps } from "next"
 import { getSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
+import useSWR from "swr"
 import Layout from "../components/Layout"
 import OnOffSwitch from "../components/OnOffSwitch"
+import Sessions from "../components/Sessions"
 import getServerInfo from "../lib/api/serverInfo"
-import { updateUser } from "../lib/api/user"
+import { fetchSessions, MangatsuSessionResponse, updateUser } from "../lib/api/user"
+import { decodeJWT } from "../lib/helpers"
 import { getValue, LocalPreferences, setValue } from "../lib/localStorage"
 import { ServerInfo } from "../types/api"
 
 interface Props {
   serverInfo: ServerInfo
+  currentSessionID: string
   token: string
   userUUID: string | null
 }
 
-export default function Personal({ serverInfo, token, userUUID }: Props) {
+export default function Personal({ serverInfo, currentSessionID, token, userUUID }: Props) {
   const [nsfwPref, setNsfwPref] = useState(false)
+  const { data, mutate } = useSWR(token, (token: string) => fetchSessions(token).then((r) => r.json()))
+  const sessions = data as MangatsuSessionResponse
 
   useEffect(() => {
     setNsfwPref(getValue(LocalPreferences.NSFWPref))
@@ -46,36 +52,42 @@ export default function Personal({ serverInfo, token, userUUID }: Props) {
 
   return (
     <Layout serverInfo={serverInfo} subtitle="Personal">
-      <h3>Personal Settings</h3>
-      <div className="flex">
-        {userUUID && (
-          <div className="inline-block p-4 rounded bg-opacity-20 bg-black">
-            <h4>User</h4>
-            <form className="max-w-xs" onSubmit={(e) => handleUserUpdate(e)}>
-              <label>New password</label>
-              <input type="password" id="password" />
-              <button
-                type="submit"
-                className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
-              >
-                Update
-              </button>
-            </form>
+      <div className="flex flex-col justify-center">
+        <h3>Personal Settings</h3>
+        <div className="grid grid-flow-col h-64">
+          {userUUID && (
+            <div className="p-4 rounded bg-opacity-20 bg-black mr-8">
+              <h4>User</h4>
+              <form onSubmit={(e) => handleUserUpdate(e)}>
+                <label>New password</label>
+                <input type="password" id="password" />
+                <button
+                  type="submit"
+                  className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+                >
+                  Update
+                </button>
+              </form>
+            </div>
+          )}
+          <div className="flex flex-col p-4 rounded bg-opacity-20 bg-black">
+            <h4>Site</h4>
+            <label>
+              Hide NSFW results by default? <OnOffSwitch checked={nsfwPref} onChange={setNsfwPref} />
+            </label>
+            <button
+              onClick={() => handlePreferences()}
+              type="submit"
+              className="mt-4 w-24 text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+            >
+              Save
+            </button>
           </div>
-        )}
-        <div className="inline-block p-4 h-64 rounded bg-opacity-20 bg-black">
-          <h4>Site</h4>
-          <label>Hide NSFW results by default?</label>
-          <br />
-          <OnOffSwitch checked={nsfwPref} onChange={setNsfwPref} />
-          <br />
-          <button
-            onClick={() => handlePreferences()}
-            type="submit"
-            className="mt-4 text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
-          >
-            Save
-          </button>
+        </div>
+        <br />
+        <div className="p-4 rounded bg-opacity-20 bg-black">
+          <h4>Sessions</h4>
+          <Sessions sessions={sessions} mutate={mutate} token={token} currentSessionID={currentSessionID} />
         </div>
       </div>
     </Layout>
@@ -93,10 +105,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 
+  const currentSessionID = decodeJWT(session.serverToken).ID
+
   const serverInfo = await getServerInfo()
   return {
     props: {
       serverInfo,
+      currentSessionID,
       token: session.serverToken,
       userUUID: session?.user?.uuid || null,
     },
