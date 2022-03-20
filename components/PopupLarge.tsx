fdf5a1d53a394d4d36a267ece032ac74232031c8
Marko Leinikka <mleinikka@gmail.com>
2022-03-20 16:34:43 +0200

    feat: Disable background scroll when large popup is open

--- components/PopupLarge.tsx
+++ components/PopupLarge.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import { ReactNode, useEffect, useState } from "react"
 import Popup from "reactjs-popup"
 
 interface Props {
@@ -7,7 +7,23 @@ interface Props {
 }
 
 const PopupLarge = ({ activator, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false)
+  useEffect(() => {
+    const html = document.getElementsByTagName("html")[0]
+
+    if (isOpen) {
+      html.classList.add("lock-scroll")
+    } else {
+      html.classList.remove("lock-scroll")
+    }
+
+    return (): void => {
+      html.classList.remove("lock-scroll")
+    }
+  }, [isOpen])
+
   return (
+    <Popup trigger={activator} modal arrow={false} onOpen={() => setIsOpen(true)} onClose={() => setIsOpen(false)}>
       <div
         className="popup-content rounded-xl bg-slate-800 w-auto h-auto m-4 px-5 py-4 flex flex-col shadow-xl overflow-y-auto"
         style={{ marginTop: 6, maxHeight: "calc(100vh - 50px)" }}
