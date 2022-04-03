59743d41da37159967d4252d9b90cbe4912d45be
Marko Leinikka <Luukuton@users.noreply.github.com>
2022-04-03 15:22:32 +0300

    feat: Add Spinner to indicate that page is loading

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -5,6 +5,7 @@ import { fetchRandomGallery } from "../../lib/api/library"
 import { Role } from "../../lib/helpers"
 import { Gallery, ServerInfo } from "../../types/api"
 import Button from "../Button"
+import Spinner from "../Spinner"
 import Scan from "./Scan"
 import User from "./User"
 
@@ -23,15 +24,19 @@ const Nav = ({ serverInfo }: { serverInfo: ServerInfo }) => {
   }
 
   return (
-    <nav className="px-8 mb-8">
-      <Button href="/" className="mx-4">
-        Library
-      </Button>
-      <Button onClick={() => handleRandom()} className="mr-2">
-        Random
-      </Button>
-      {session?.serverToken && isAdmin && <Scan token={session?.serverToken} />}
-      <User session={session} isAdmin={isAdmin} status={status} serverInfo={serverInfo} />
+    <nav className="px-8 mb-8 flex flex-row justify-between">
+      <Spinner />
+      <span className="flex">
+        <Button href="/" className="mx-4">
+          Library
+        </Button>
+        <Button onClick={() => handleRandom()} className="mr-2">
+          Random
+        </Button>
+
+        {session?.serverToken && isAdmin && <Scan token={session?.serverToken} />}
+        <User session={session} isAdmin={isAdmin} status={status} serverInfo={serverInfo} />
+      </span>
     </nav>
   )
 }
