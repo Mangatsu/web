df324d637a3bf1fa6728206db7b8ba0ffa22ca87
Marko Leinikka <mleinikka@gmail.com>
2022-04-15 18:06:49 +0300

    fix: Entrypoint echo message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Check that we have NEXT_PUBLIC_MANGATSU_API_URL vars"
+echo "Checking that NEXT_PUBLIC_MANGATSU_API_URL var exists"
 test -n "$NEXT_PUBLIC_MANGATSU_API_URL"
 
 find /mtsu-web/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_MANGATSU_API_URL#$NEXT_PUBLIC_MANGATSU_API_URL#g"
