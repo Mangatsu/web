96dd3210b13addfde041887524ac03074cdba56f
Marko Leinikka <mleinikka@gmail.com>
2022-07-24 14:27:06 +0300

    ci: Change standalone output config option to the new one

--- next.config.js
+++ next.config.js
@@ -8,9 +8,7 @@ const nextConfig = {
     formats: ["image/webp"],
     minimumCacheTTL: 60 * 60 * 24 * 30,
   },
-  experimental: {
-    outputStandalone: true,
-  },
+  output: "standalone",
 }
 
 module.exports = nextConfig
