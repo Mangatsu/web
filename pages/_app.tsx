99303ab1a03d0b855bfee9bb3e3d5fb61dc0a6a5
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-10-24 06:20:28 +0700

    (fix): Type error when build on new next.js version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import "react-toastify/dist/ReactToastify.css"
 import ExpiredLogin from "../components/ExpiredLogin"
 import "../styles/globals.css"
 
-function Mangatsu({ Component, pageProps }: AppProps) {
+function Mangatsu({ Component, pageProps }: AppProps<{ session: Session; }>) {
   return (
     <SessionProvider session={pageProps.session}>
       <ExpiredLogin />
