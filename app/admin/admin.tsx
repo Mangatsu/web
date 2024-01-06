547a1fdb5183a0c0371776c464078b36d5f97b94
Marko Leinikka <mleinikka@gmail.com>
2024-01-06 22:45:13 +0900

    fix: admin page double nav

--- app/admin/admin.tsx
+++ app/admin/admin.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { notFound } from "next/navigation"
 import useSWR from "swr"
-import Layout from "../../components/Layout"
 import NewUser from "../../components/NewUser"
 import Users from "../../components/Users"
 import { APIPathsV1, swrFetcher } from "../../lib/api/other"
@@ -21,19 +20,17 @@ export default function Admin() {
 
   const users = data?.Data ? (data as MangatsuUserResponse) : null
   return (
-    <Layout subtitle="Administrative">
-      <div className="flex flex-col justify-center ">
-        <br />
-        <h3>Administrative settings</h3>
-        <div className="p-4 rounded bg-opacity-20 bg-black">
-          <h4>New user</h4>
-          <NewUser mutate={mutate} />
-        </div>
-        <div className="p-4 rounded bg-opacity-20 bg-black">
-          <h4>Users</h4>
-          {users && <Users users={users} userUUID={userUUID} mutate={mutate} />}
-        </div>
+    <div className="flex flex-col justify-center ">
+      <br />
+      <h3>Administrative settings</h3>
+      <div className="p-4 rounded bg-opacity-20 bg-black">
+        <h4>New user</h4>
+        <NewUser mutate={mutate} />
       </div>
-    </Layout>
+      <div className="p-4 rounded bg-opacity-20 bg-black">
+        <h4>Users</h4>
+        {users && <Users users={users} userUUID={userUUID} mutate={mutate} />}
+      </div>
+    </div>
   )
 }
