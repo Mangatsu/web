1e09b760b97708f2394189ecd8ef83dd93a12ee4
Marko Leinikka <mleinikka@gmail.com>
2024-01-09 03:30:23 +0900

    fix: remove unused enum that caused TS errors

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,12 +1,6 @@
 import { Metadata } from "next"
 import StatusPage from "./status"
 
-export enum ProcessingStatus {
-  ScanGalleries,
-  GenerateMetadata,
-  GenerateThumbnails,
-}
-
 export const metadata: Metadata = {
   title: "Mangatsu | status",
   description: "Processing status",
