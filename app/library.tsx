1683282e2c28e48f46ba54b074930071a2753b0e
Marko Leinikka <mleinikka@gmail.com>
2024-01-21 06:56:47 +0900

    fix: remove debug log

--- app/library.tsx
+++ app/library.tsx
@@ -44,7 +44,6 @@ function Library() {
   // This should actually be called "mightHaveMore", as in rare cases the data ends exactly on the offset
   // which results one extra request being made. (results % offset === 0)
   const hasMoreGalleries = gData && gData[gData.length - 1]?.Count === RESULT_LIMIT
-  console.log(gData)
 
   // TODO: Grid masonry when major browsers support it (https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Masonry_Layout)
   return (
