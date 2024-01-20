6fe52e8a8ee43cbea86fb56fbbdc601a5049b0a5
Marko Leinikka <mleinikka@gmail.com>
2024-01-21 04:24:35 +0900

    chore: remove commented code

--- app/page.tsx
+++ app/page.tsx
@@ -7,31 +7,5 @@ export const metadata: Metadata = {
 }
 
 export default async function Page() {
-  /*
-  async function getServerInfo() {
-    let serverInfo: ServerInfo = await fetchServerInfo()
-    if (!serverInfo) {
-      serverInfo = {
-        APIVersion: 0,
-        ServerVersion: "Unknown",
-        Visibility: Visibility.Private,
-        Registrations: false,
-      }
-    }
-
-    const cookie = headers().get("headers")
-    const jwtCookie = parseCookieHeader("mtsu.jwt", cookie)
-    const publicAccess = serverInfo.Visibility === Visibility.Public
-    if (!publicAccess && !jwtCookie) {
-      // to login page
-    }
-
-    return {
-      props: {},
-    }
-  }
-
-   await getServerInfo()
-  */
   return <Library />
 }
