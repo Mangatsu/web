ae26bacacb991af45d1287d9a1c120bd42e9cea5
Marko Leinikka <mleinikka@gmail.com>
2024-03-21 02:29:47 +0900

    feat: add AVIF support, decrease min cache TTL to 1 day and improve remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const imageDomain = process.env.NEXT_MANGATSU_IMAGE_HOSTNAME
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      ...(imageDomain
-        ? [
-            {
-              protocol: "https",
-              hostname: imageDomain,
-              pathname: "**",
-            },
-          ]
-        : []),
-      { protocol: "http", hostname: "localhost", port: "5050", pathname: "**" },
-      { protocol: "http", hostname: "127.0.0.1", port: "5050", pathname: "**" },
+      ...(imageDomain ? [{ hostname: imageDomain, pathname: "**" }] : []),
+      { hostname: "localhost", port: "**", pathname: "**" },
+      { hostname: "127.0.0.1", port: "**", pathname: "**" },
     ],
-    formats: ["image/webp"],
-    minimumCacheTTL: 60 * 60 * 24 * 30,
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24,
   },
   output: "standalone",
 }
